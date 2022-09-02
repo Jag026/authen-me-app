cd292aba8be248b65be43ce5790a8ff61eec1c81
Drew <DGriffin@appacademy.io>
2022-09-02 16:00:43 -0500

    Added login page

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import sessionReducer from './session';
+
 const rootReducer = combineReducers({
+  session: sessionReducer,
 });
 
 let enhancer;
@@ -18,5 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
+
 
 export default configureStore;
